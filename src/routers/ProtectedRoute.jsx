3d11eed04148ad74c8a4793088c1a43bc27f5c58
Lucas Viana <vianalucas812@gmail.com>
2024-10-15 16:14:25 -0300

    fix: retirando token do console

--- src/routers/ProtectedRoute.jsx
+++ src/routers/ProtectedRoute.jsx
@@ -4,8 +4,6 @@ import { Navigate } from 'react-router-dom';
 const ProtectedRoute = ({ children }) => {
   const token = localStorage.getItem('access_token');
 
-  console.log(token)
-
   if (!token) {
     // Se o token não estiver presente, redireciona para a página de login
     return <Navigate to="/login" />;
